266cc9c8ed4cda9d132ac368b73f209ece3c1343
Julian <mail@julianrijken.com>
2025-03-22 12:30:15 +0100

    added extra lift

--- vehicle.gd
+++ vehicle.gd
@@ -27,8 +27,8 @@ func _physics_process(delta: float) -> void:
 			continue
 			
 		var hitPoint = collision["position"]
-		DebugDraw3D.draw_line(query.from, hitPoint, Color(1, 1, 0))
-		#DebugDraw3D.draw_sphere(hitPoint,0.05)
+		DebugDraw3D.draw_line(query.from, hitPoint, Color.DIM_GRAY)
+		DebugDraw3D.draw_sphere(hitPoint,0.025,Color.DIM_GRAY)
 		wheelPoints.push_back(hitPoint)
 		
 		if hitPoint.y > wheelPoints[highestWheelIndex].y:
@@ -40,40 +40,76 @@ func _physics_process(delta: float) -> void:
 			
 		wheelIndex += 1
 		
+		
 	if wheelPoints.is_empty() or wheelPoints.size() != 4:
 		return
 		
 	
-	var lowestWheel = wheelPoints[lowestWheelIndex]
-	var oppositeWheel = wheelPoints[(lowestWheelIndex + 2)%4]
-	var sideWheel1 = wheelPoints[(lowestWheelIndex + 1)%4]
-	var sideWheel2 = wheelPoints[(lowestWheelIndex + 3)%4]
+	var lowestWheel: Vector3 = wheelPoints[lowestWheelIndex]
+	var oppositeWheel: Vector3 = wheelPoints[(lowestWheelIndex + 2)%4]
 	
-	var dir1 = (sideWheel1 - oppositeWheel)
-	var dir2 = (sideWheel2 - oppositeWheel)
-	var dir1n = (sideWheel1 - oppositeWheel).normalized()
-	var dir2n = (sideWheel2 - oppositeWheel).normalized()
+	var closeSide = wheelPoints[(lowestWheelIndex + 1)%4]
+	var farSide = wheelPoints[(lowestWheelIndex + 3)%4]
 	
-	var foundWheel = oppositeWheel + dir1 + dir2;
-	DebugDraw3D.draw_sphere(foundWheel,0.03,Color.PALE_VIOLET_RED)
+	if (oppositeWheel - closeSide).length_squared() > (oppositeWheel - farSide).length_squared():
+		var oldCloseSide = closeSide
+		closeSide = farSide
+		farSide = oldCloseSide
 	
-	var diff = max(lowestWheel.y - foundWheel.y,0)
-	foundWheel.y += diff
+	DebugDraw3D.draw_sphere(oppositeWheel,0.05,Color.LIME_GREEN)
+	DebugDraw3D.draw_sphere(lowestWheel,0.05,Color.ORANGE_RED)
 	
-	var highestSide = sideWheel1 if sideWheel1.y > sideWheel2.y else sideWheel2
-	highestSide.y += diff;
-	DebugDraw3D.draw_sphere(highestSide,0.05,Color.AQUA)
+	#DebugDraw3D.draw_sphere(closeSide,0.05,Color.YELLOW)
+	#DebugDraw3D.draw_sphere(farSide,0.05,Color.ROYAL_BLUE)
+
+	var closeDirection = (closeSide - oppositeWheel)
+	var farDirection = (farSide - oppositeWheel)
 	
-	var up = dir1n.cross(dir2n)
-	DebugDraw3D.draw_arrow(global_position,global_position + up,Color.RED,0.1)
+	var foundWheel = oppositeWheel + closeDirection + farDirection
 	
-	DebugDraw3D.draw_line(oppositeWheel,sideWheel1,Color.MEDIUM_SLATE_BLUE)
-	DebugDraw3D.draw_line(oppositeWheel,sideWheel2,Color.MEDIUM_SLATE_BLUE)
-	DebugDraw3D.draw_line(sideWheel1,foundWheel,Color.CORAL)
-	DebugDraw3D.draw_line(sideWheel2,foundWheel,Color.CORAL)
+
+	var diff = foundWheel - lowestWheel
+	
+	# Needs to up car wheel
+	# then we move close point up
+	if diff.y < 0:
+		var liftedCloseSide = closeSide - diff
+		#DebugDraw3D.clear_all()
+		DebugDraw3D.draw_sphere(foundWheel,0.05,Color.PALE_VIOLET_RED)
+		DebugDraw3D.draw_sphere(liftedCloseSide,0.05,Color.PURPLE)
+		
+		DebugDraw3D.draw_line(oppositeWheel, liftedCloseSide,Color.YELLOW)
+		DebugDraw3D.draw_line(oppositeWheel, farSide,Color.YELLOW)
+		DebugDraw3D.draw_line(lowestWheel, liftedCloseSide,Color.YELLOW)
+		DebugDraw3D.draw_line(lowestWheel, farSide,Color.YELLOW)
+	else:
+		DebugDraw3D.draw_arrow(oppositeWheel,oppositeWheel + closeDirection,Color.YELLOW,0.1,true)
+		DebugDraw3D.draw_arrow(oppositeWheel,oppositeWheel + farDirection,Color.ROYAL_BLUE,0.1,true)
+		DebugDraw3D.draw_arrow(oppositeWheel + closeDirection,foundWheel,Color.GRAY,0.1,true)
+		DebugDraw3D.draw_arrow(oppositeWheel + farDirection,foundWheel,Color.GRAY,0.1,true)
+	
+		
+		
 	
 	
-	DebugDraw3D.draw_sphere(oppositeWheel,0.05,Color.GREEN)
-	DebugDraw3D.draw_sphere(sideWheel1,0.05,Color.REBECCA_PURPLE)
-	DebugDraw3D.draw_sphere(sideWheel2,0.05,Color.BLUE_VIOLET)
-	DebugDraw3D.draw_sphere(foundWheel,0.05,Color.RED)
+	
+	#
+	#foundWheel.y += diff
+	#
+	#var highestSide = sideWheel1 if sideWheel1.y > sideWheel2.y else sideWheel2
+	#highestSide.y += diff;
+	#DebugDraw3D.draw_sphere(highestSide,0.05,Color.AQUA)
+	#
+	#var up = dir1n.cross(dir2n)
+	#DebugDraw3D.draw_arrow(global_position,global_position + up,Color.RED,0.1)
+	#
+	#DebugDraw3D.draw_line(oppositeWheel,sideWheel1,Color.MEDIUM_SLATE_BLUE)
+	#DebugDraw3D.draw_line(oppositeWheel,sideWheel2,Color.MEDIUM_SLATE_BLUE)
+	#DebugDraw3D.draw_line(sideWheel1,foundWheel,Color.CORAL)
+	#DebugDraw3D.draw_line(sideWheel2,foundWheel,Color.CORAL)
+	
+	
+	#DebugDraw3D.draw_sphere(oppositeWheel,0.05,Color.GREEN)
+	#DebugDraw3D.draw_sphere(sideWheel1,0.05,Color.REBECCA_PURPLE)
+	#DebugDraw3D.draw_sphere(sideWheel2,0.05,Color.BLUE_VIOLET)
+	#DebugDraw3D.draw_sphere(foundWheel,0.05,Color.RED)
